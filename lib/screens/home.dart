1c6d8f124f0eb8f265865f62586b84865c2f69d5
msploit0x00 <minasploit1@outlook.com>
2023-09-29 23:33:09 +0300

    kolo ta7t el control

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,18 +1,47 @@
-
-
-
 import 'package:flutter/material.dart';
 
 class MyHome extends StatelessWidget {
   static const String routName = "home";
-  
-  const MyHome({super.key});
 
+  const MyHome({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.green,
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("EXAM"),
+          centerTitle: true,
+          bottom: TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(Icons.home),
+              ),
+              Tab(
+                icon: Icon(Icons.home),
+              ),
+              Tab(
+                icon: Icon(Icons.home),
+              )
+            ],
+          ),
+        ),
+        body: TabBarView(children: [
+          Container(
+            color: Colors.yellow,
+            child: Icon(Icons.home),
+          ),
+           Container(
+            color: Colors.tealAccent,
+            child: Icon(Icons.home),
+          ),
+           Container(
+            color: Colors.yellowAccent,
+            child: Icon(Icons.home),
+          ),
+        ]),
+      ),
     );
   }
 }

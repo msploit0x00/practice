7ef26d6296ba4e8c7109f08a72a87f8fb37e50d5
msploit0x00 <minasploit1@outlook.com>
2023-09-29 22:40:53 +0300

    session_exam

---
+++ lib/screens/home.dart
@@ -0,0 +1,18 @@
+
+
+
+import 'package:flutter/material.dart';
+
+class MyHome extends StatelessWidget {
+  static const String routName = "home";
+  
+  const MyHome({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green,
+    );
+  }
+}
